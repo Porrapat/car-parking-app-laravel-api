4229dbe76cd409f277a95d04043a29e6e19da402
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2023-05-08 14:49:41 +0700

    Topic 5 OK

---
+++ app/Http/Resources/VehicleResource.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class VehicleResource extends JsonResource
+{
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'plate_number' => $this->plate_number,
+        ];
+    }
+}
